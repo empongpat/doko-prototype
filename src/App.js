c6a3c4269f84de6aef911b8385f04162e9eabeaa
shinzenkoru <empongpat@gmail.com>
2018-03-29 08:46:50 +0700

    Add SASS support and sass script

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import { Link } from 'react-router-dom'
 import logo from './logo.svg';
-import './App.css';
 
 const Page = ({ title }) => (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './assets/styles/css/index.css';
 
 import { BrowserRouter }  from 'react-router-dom'
 
