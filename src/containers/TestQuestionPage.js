b03670065b18c73360a909a6c4a82f1194c68af5
Pasinb <panbemail@gmail.com>
2018-04-01 09:28:24 +0700

    Connect question API

--- src/containers/TestQuestionPage.js
+++ src/containers/TestQuestionPage.js
@@ -18,16 +18,26 @@ export default class TestQuestionPage extends React.Component {
     this.state = {
       editorValue: "",
       evalValue: "",
-      isTopicMenuExpanded: false
+      isTopicMenuExpanded: false,
+      questionList: [],
+      currentQuestionIdx: 0
     }
   }
 
   componentWillMount() {
-    axios.get('https://139.59.246.127/questions')
+    var thisObj = this
+    axios.get('http://139.59.246.127:4000/questions')
       .then(function (response) {
+        console.log("Questions")
         console.log(response);
+        console.log(response.data.questions)
+        thisObj.setState({
+          questionList: response.data.questions,
+          editorValue: response.data.questions[0].editorText
+        })
       })
       .catch(function (error) {
+        console.log("Questions err")
         console.log(error);
       })
   }
@@ -43,22 +53,99 @@ export default class TestQuestionPage extends React.Component {
     console.log(value)
     this.setState({
       editorValue: value,
-      evalValue: value + " answer"
+      // evalValue: value + " answer"
     })
   }
 
-  submitOnClick = () => {
-    swal("Good job!", "You clicked the button!", "success");
+  prevOnClick = () => {
+    if (this.state.currentQuestionIdx == 0) {
+      return
+    }
+    const prevQuestionIdx = this.state.currentQuestionIdx - 1
+    this.setState({
+      currentQuestionIdx: prevQuestionIdx,
+      editorValue: this.state.questionList[prevQuestionIdx].editorText
+    })
   }
 
+  submitAnswer = () => {
+    axios.post('http://139.59.246.127:4000/eval', {
+      'code': this.state.editorValue,
+      'question': this.state.currentQuestionIdx
+    })
+      .then(function (response) {
+        console.log("Eval res")
+        console.log(response.data);
+        return response.data
+      })
+      .catch(function (error) {
+        console.log("Eval err")
+        console.log(error);
+      })
+  }
+
+  moveToQuestion = (DeltaIdx) => {
+    const nextQuestionIdx = this.state.currentQuestionIdx + DeltaIdx
+    if (nextQuestionIdx == this.state.questionList.length) {
+      // finished all question
+      return
+    }
+    if (nextQuestionIdx < 0){
+      return
+    }
+    this.setState({
+      currentQuestionIdx: nextQuestionIdx,
+      editorValue: this.state.questionList[nextQuestionIdx].editorText,
+      evalValue: ""
+    })
+  }
+
+  nextOnClick = () => {
+    this.moveToQuestion(1)
+  }
+
+  runOnClick = () => {
+    var thisObj = this
+    axios.post('http://139.59.246.127:4000/eval', {
+      'code': this.state.editorValue,
+      'question': this.state.currentQuestionIdx
+    })
+      .then(function (response) {
+        const evalResponse = response.data
+        if (evalResponse.success) {
+          swal(evalResponse.message, "", "success")
+          thisObj.setState({
+            evalValue: evalResponse.stdout
+          })
+
+        } else {
+          swal(evalResponse.message, "", "error")
+          thisObj.setState({
+            evalValue: evalResponse.stderr
+          })
+        }
+      })
+      .catch(function (error) {
+        console.log("Eval err")
+        console.log(error);
+      })
+  }
+
+
   render() {
-    console.log(this.state.isTopicMenuExpanded)
+    console.log('ren')
+    console.log(this.state.questionList)
     return (
       <div className="theme-gray-bg h-100">
         <div className="row pt-3 m-0" style={ { "height": "calc(100% - 73px)" } }>
           <div className="col-4">
-            <div className="box" >Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text Question text
-             <div className="topic-menu" style={ this.state.isTopicMenuExpanded ? { "height": "50%" } : { "height": "0%", "border": 0, "padding": 0 } }>
+            <div className="box" >
+              { this.state.questionList.length > 0 ?
+                (<div><h3>{ this.state.questionList[this.state.currentQuestionIdx].title }</h3>
+                  <span dangerouslySetInnerHTML={ { __html: this.state.questionList[this.state.currentQuestionIdx].text } }></span></div>) : null
+              }
+
+              <div className="topic-menu" style={ this.state.isTopicMenuExpanded ? { "height": "50%" } : { "height": "0%", "border": 0, "padding": 0 } }>
                 <span onClick={ this.toggleTopicMenu }><i className="fas fa-angle-down expand-button active"></i></span>
                 { this.state.isTopicMenuExpanded ?
                   <ul>
@@ -85,30 +172,22 @@ export default class TestQuestionPage extends React.Component {
                   { this.state.evalValue }
                 </div>
               </div>
-              {/* <div className="col-12 mt-3">
-                <div className="box">
-                  <Editor onChange={ this.editorOnChange } value={ this.state.editorValue } />
-                </div>
-              </div> */}
             </div>
             <div className="row" style={ { "height": "70%" } }>
-              {/* <div className="col-12">
-                <div className="box">
-                  { this.state.evalValue }
-                </div>
-              </div> */}
               <div className="col-12 mt-3">
                 <div className="box">
                   <Editor onChange={ this.editorOnChange } value={ this.state.editorValue } />
-                  <Button color="success" style={ {"position": "absolute", "bottom": 8, "right":8, "zIndex": 999} }>Run</Button>
+                  { this.state.questionList.length > 0 ?
+                    <Button onClick={ this.runOnClick } disabled={ this.state.questionList[this.state.currentQuestionIdx].haveQuestion !== true } color="success" style={ { "position": "absolute", "bottom": 8, "right": 8, "zIndex": 999 } }>Run</Button>
+                    : null }
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="row m-0 p-3 justify-content-center">
-          <button className="btn-question-footer mr-3 px-5 py-1">BACK</button>
-          <button className="btn-question-footer ml-3 px-5 py-1" onClick={ this.submitOnClick }>NEXT</button>
+          <button className="btn-question-footer mr-3 px-5 py-1" onClick={ this.prevOnClick }>BACK</button>
+          <button className="btn-question-footer ml-3 px-5 py-1" onClick={ this.nextOnClick }>NEXT</button>
         </div>
       </div>)
   }
